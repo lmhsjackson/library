550a85e4ed4d7558a448dd74edfbbf70f48a0a6e
Jose Piccioni <josepiccioni@gmail.com>
2018-12-19 10:27:18 -0300

    Adding active admin

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'devise'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'activeadmin'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,6 @@
-class Admin < ActiveRecord::Base
+class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 end
